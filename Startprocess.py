ca6bf9f56aa71e4122e032b72cfaff61036ee1b0
Brandon <brandongohwh@hotmail.com>
2020-01-03 01:34:54 +0800

    test3

--- Startprocess.py
+++ Startprocess.py
@@ -447,7 +447,7 @@ def preCheck():
         os.environ['WINEARCH']= "win32"
         os.environ['WINEPREFIX']=os.path.expanduser("~")+os.path.sep+".winedotnet"
         #subprocess.call(['wineboot','-u'])
-        if not os.path.exists(os.path.join(os.path.expanduser("~"), '.winedotnet','drive_c','windows', 'Microsoft.NET', 'Framework', 'v4.0.30319')):
+        if os.path.exists(os.path.join(os.path.expanduser("~"), '.winedotnet','drive_c','windows', 'Microsoft.NET', 'Framework', 'v4.0.30319')):
             subprocess.call(['wine','installer/dotnetfx45_full_x86_x64.exe'])
         #subprocess.call(['which','wine'])
         #sys.exit(0)
